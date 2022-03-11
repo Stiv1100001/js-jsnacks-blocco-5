3d7b6202b82719be44002dc853b033835084e8ed
Stiv1100001 <bichicchi.stefano@gmail.com>
2022-03-11 12:34:29 +0100

    Snack 3

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,1 +1,10 @@
-const result = document.getElementById(`main`);
+const result = document.getElementById('result');
+
+const names = ['piPPo', 'plUto', 'paperiNO'];
+const newNames = [];
+
+names.forEach((name) => {
+  newNames.push(name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
+});
+
+result.innerHTML = newNames.join(', ');
