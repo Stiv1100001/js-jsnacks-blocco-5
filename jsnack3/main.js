072c2d2594773f040d30e0ae761ce038865188c9
Stiv1100001 <bichicchi.stefano@gmail.com>
2022-03-11 12:35:31 +0100

    better way

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,10 +1,9 @@
 const result = document.getElementById('result');
 
 const names = ['piPPo', 'plUto', 'paperiNO'];
-const newNames = [];
 
-names.forEach((name) => {
-  newNames.push(name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
+const newNames = names.map((name) => {
+  return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
 });
 
 result.innerHTML = newNames.join(', ');
